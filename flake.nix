8c799be78f7020888393a9cc85188369f6be05fd
OwO Swordsman <owoswordsman@gmail.com>
2023-07-18 16:18:04 -0400

    Add prettier flakes

--- flake.nix
+++ flake.nix
@@ -42,9 +42,46 @@
           ++ pkgs.lib.optionals pkgs.stdenv.isDarwin macosDependencies
           ++ pkgs.lib.optionals pkgs.stdenv.isDarwin macosFrameworks;
       in rec {
-        packages.default = naersk'.buildPackage {
+        packages.default = packages.rustyaura;
+
+        packages.rustyaura = naersk'.buildPackage {
           src = ./.;
-          buildInputs = dependencies ++ [];
+          buildInputs = dependencies ++ [packages.prettier];
+        };
+
+        packages.prettier-plugin-tailwindcss = pkgs.buildNpmPackage rec {
+          pname = "prettier-plugin-tailwindcss";
+          version = "0.4.1";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "tailwindlabs";
+            repo = pname;
+            rev = "v${version}";
+            hash = "sha256-yc434+Yhhzw1ivz+oAgLCPCndVb0g+KucPxPknGlRp4=";
+          };
+
+          npmDepsHash = "sha256-b2ioMu+9+j6xPe+0hqu9JrZfVW5eH4b3HISeTYeugeY=";
+        };
+
+        packages.prettier = pkgs.stdenv.mkDerivation {
+          name = "prettier";
+
+          src = ./.;
+          nativeBuildInputs = with pkgs; [makeWrapper];
+
+          buildPhase = ''
+            mkdir -p $out/lib/node_modules
+            cp -r ${pkgs.nodePackages.prettier}/lib/node_modules/* $out/lib/node_modules
+            cp -r ${packages.prettier-plugin-tailwindcss}/lib/node_modules/* $out/lib/node_modules
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            makeWrapper $out/lib/node_modules/prettier/bin-prettier.js $out/bin/prettier \
+              --add-flags "--tab-width 4" \
+              --add-flags "--plugin $out/lib/node_modules/prettier-plugin-tailwindcss" \
+              --add-flags "--plugin $out/lib/node_modules/prettier"
+          '';
         };
 
         devShells.default = pkgs.mkShell {
