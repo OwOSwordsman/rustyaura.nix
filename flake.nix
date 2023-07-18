5be7b52f1bc332816c7a20379fcd26c41ff0e550
OwO Swordsman <owoswordsman@gmail.com>
2023-07-18 16:15:37 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    naersk.url = "github:nix-community/naersk";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    naersk,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        # https://github.com/oxalica/rust-overlay#cheat-sheet-common-usage-of-rust-bin
+        toolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = ["rust-src"];
+        };
+
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        sharedDependencies = with pkgs; [toolchain sccache];
+        linuxDependencies = with pkgs; [mold clang];
+        macosDependencies = with pkgs; [];
+        macosFrameworks = with pkgs.darwin.apple_sdk.frameworks; [];
+
+        dependencies =
+          sharedDependencies
+          ++ pkgs.lib.optionals pkgs.stdenv.isLinux linuxDependencies
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin macosDependencies
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin macosFrameworks;
+      in rec {
+        packages.default = naersk'.buildPackage {
+          src = ./.;
+          buildInputs = dependencies ++ [];
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; dependencies ++ [];
+          env = {
+            LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath dependencies;
+          };
+        };
+      }
+    );
+}
