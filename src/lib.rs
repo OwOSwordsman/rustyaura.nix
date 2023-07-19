39951e06cdb90eb74c8adb0389809c8653dd822a
OwO Swordsman <owoswordsman@gmail.com>
2023-07-19 17:50:24 -0400

    Verify generated code

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     let formatted_rust = run_rustfmt(&input)?;
     let (surrounding_rust_code, css_classes) = parser::parse_classes(&formatted_rust);
     let formatted_classes = prettier::format_classes(&surrounding_rust_code, &css_classes)?;
+    verify_code(&formatted_classes)?;
     Ok(())
 }
 
@@ -26,6 +27,18 @@ fn run_rustfmt(code: &str) -> Result<String, Box<dyn Error>> {
     let output = rustfmt.wait_with_output()?;
     if output.status.success() {
         Ok(String::from_utf8(output.stdout)?)
+    } else {
+        Err("rustfmt failed".into())
+    }
+}
+
+fn verify_code(code: &str) -> Result<(), Box<dyn Error>> {
+    let mut rustfmt = Command::new("rustfmt").stdin(Stdio::piped()).spawn()?;
+    writeln!(rustfmt.stdin.take().unwrap(), "{}", code)?;
+
+    let output = rustfmt.wait_with_output()?;
+    if output.status.success() {
+        Ok(())
     } else {
         Err("rustfmt failed".into())
     }
